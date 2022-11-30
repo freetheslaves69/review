4b313d8edebf480203f428d81cf82d30499669d2
freetheslaves69 <116140783+freetheslaves69@users.noreply.github.com>
2022-11-30 12:12:06 +0700

    Add files via upload

---
+++ review.py
@@ -0,0 +1,15 @@
+football_team = []
+
+#append 11 players with for loop, ask user for players name 
+for i in range (11):
+    name = input("add a player  to the football team: ")
+    football_team.append(name)
+print(football_team)
+
+repeat = 'Y'
+while repeat == 'Y'
+#TO DO: edit an item,
+#TO DO: delete item.
+#Ask user which item to change/delete.
+    repeat = input ('do you want to edit or delete name')
+print(football_team)
